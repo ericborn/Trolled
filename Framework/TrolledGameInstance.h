18c38cf216c7e37644525cf4071627b59bd877f8
Eric Born <eric.born85@gmail.com>
2020-08-01 19:34:14 -0700

    Create framework files for game mode, state, etc.

---
+++ Framework/TrolledGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TrolledGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TROLLED_API UTrolledGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
