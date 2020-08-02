b307f11dc3b9c68c1b96d56b1fb8d5417f3c2788
Eric Born <eric.born85@gmail.com>
2020-08-01 19:34:45 -0700

    Move character files

---
+++ Player/TrolledPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "TrolledPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TROLLED_API ATrolledPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
---
+++ TrolledGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TrolledGameMode.h"
+#include "TrolledCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATrolledGameMode::ATrolledGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
