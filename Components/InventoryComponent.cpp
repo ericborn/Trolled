66d5b67e4352a10323f0cf4cc9a290f113986f94
Eric Born <eric.born85@gmail.com>
2020-08-03 12:40:47 -0700

    building inventory functionality

--- Components/InventoryComponent.cpp
+++ Components/InventoryComponent.cpp
@@ -1,16 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "InventoryComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/ActorChannel.h"
 
 // Sets default values for this component's properties
 UInventoryComponent::UInventoryComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	// Replicates items in inventory to server, so that all other players can access those values.
+	// useful for keeping loot on a corpse and preventing players from modding inventory without server knowing
+	SetIsReplicated(true);
 
-	// ...
+
 }
 
 
@@ -30,5 +31,40 @@ void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FA
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+void UInventoryComponent::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty> & OutLifetimeProps) const 
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    // replicates and makes the server manage the quantity value
+    DOREPLIFETIME(UInventoryComponent, InventoryArray);
+}
+
+bool UInventoryComponent::ReplicateSubobjects(class UActorChannel *Channel, class FOutBunch *Bunch, FReplicationFlags *RepFlags) 
+{
+	// true when a modification has been made to actor channel
+	bool bWroteSomething = Super::ReplicateSubobjects(Channel, Bunch, RepFlags);
+
+	// check if the array of items needs to replicate
+	if (Channel->KeyNeedsToReplicate(0, ReplicatedItemsKey))
+	{
+		// loops through inventory array
+		for (auto& Item : InventoryArray)
+		{
+			// replicate if item marked as needs to replicate
+			if (Channel->KeyNeedsToReplicate(Item->GetUniqueID(), Item->RepKey))
+			{
+				bWroteSomething |= Channel->ReplicateSubobject(Item, *Bunch, *RepFlags);
+			}
+		}
+	}
+	// returns a bool
+	return bWroteSomething;
+}
+
+void UInventoryComponent::OnRep_Items() 
+{
+	
 }
 
--- Components/InventoryComponent.h
+++ Components/InventoryComponent.h
@@ -3,9 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Trolled/Items/BaseItem.h"
 #include "Components/ActorComponent.h"
 #include "InventoryComponent.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TROLLED_API UInventoryComponent : public UActorComponent
@@ -24,5 +24,31 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+protected:
+	// array for the current inventory, replicated to server
+	UPROPERTY(ReplicatedUsing = OnRep_Items, VisibleAnywhere, Category = "Inventory")
+	TArray<class UBaseItem*> InventoryArray;
+
+	// total inventory slots, increased with bags
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory", meta = (ClampMin = 0, ClampMax = 200))
+	int32 InventoryCapacity;
+
+	// weight capacity
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory")
+	float WeightCapacity;
+
+	//networking for UObject
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual bool ReplicateSubobjects(class UActorChannel *Channel, class FOutBunch *Bunch, FReplicationFlags *RepFlags);
+
+
+private:
+	// used to refresh UI when items gained, used, equipped, etc
+	UFUNCTION()
+	void OnRep_Items();
+
+	// used to control when an item needs to be replicated
+	UPROPERTY()
+	int32 ReplicatedItemsKey;
 		
 };
