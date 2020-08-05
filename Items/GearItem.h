afc014e2bfd11ee40a2d43c9b3c0d232456aee8b
Eric Born <eric.born85@gmail.com>
2020-08-04 18:49:00 -0700

    Create GearItem.h

---
+++ Items/GearItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Trolled/Items/EquippableItem.h"
+#include "GearItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TROLLED_API UGearItem : public UEquippableItem
+{
+	GENERATED_BODY()
+	
+};
