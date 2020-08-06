b7993b3dbabf480c038b9027e0106db43b72b73b
Eric Born <eric.born85@gmail.com>
2020-08-05 20:50:51 -0700

    Implement world spawns

--- World/ItemSpawn.cpp
+++ World/ItemSpawn.cpp
@@ -2,4 +2,112 @@
 
 
 #include "ItemSpawn.h"
+#include "Trolled/World/PickupBase.h"
+#include "Trolled/Items/BaseItem.h"
 
+AItemSpawn::AItemSpawn()
+{
+	// no tick necessary, item sits stationary until interacted with
+    PrimaryActorTick.bCanEverTick = false;
+	
+    // prevents loading from clients, only server, then replicates them down to clients
+    bNetLoadOnClient = false;
+
+    // range of time for the respawn to happen, current 10-30 seconds
+	RespawnRange = FIntPoint(10, 30);
+}
+
+void AItemSpawn::BeginPlay() 
+{
+    Super::BeginPlay();
+
+    // if server, spawn item
+	if (HasAuthority())
+	{
+		SpawnItem();
+	}
+}
+
+void AItemSpawn::SpawnItem() 
+{
+    // check that we're server and valid loot table
+    if (HasAuthority() && LootTable)
+	{
+        // create an array then load it with loot table items
+        TArray<FLootTableRow*> SpawnItems;
+		LootTable->GetAllRows("", SpawnItems);
+
+        // select a random loot table roll
+		const FLootTableRow* LootRow = SpawnItems[FMath::RandRange(0, SpawnItems.Num() - 1)];
+
+        // check its valid
+		ensure(LootRow);
+
+        // roll for probability
+		float ProbabilityRoll = FMath::FRandRange(0.f, 1.f);
+
+        // while loot rows probability is less than the roll
+		while (ProbabilityRoll > LootRow->Probability)
+		{
+            // grab a new item and make a new roll until its not true
+			LootRow = SpawnItems[FMath::RandRange(0, SpawnItems.Num() - 1)];
+			ProbabilityRoll = FMath::FRandRange(0.f, 1.f);
+		}
+
+        // check the row is valid and has items to spawn of type pickup
+        if (LootRow && LootRow->Items.Num() && PickupClass)
+		{
+            // setup for multi item spawning being in a circular shape
+            float Angle = 0.f;
+
+            // loop through the items
+            for (auto& ItemClass : LootRow->Items)
+			{
+				// create an offset from the original spawn location so items dont stack on top of each other
+                const FVector LocationOffset = FVector(FMath::Cos(Angle), FMath::Sin(Angle), 0.f) * 50.f;
+
+                // define spawn parameters, prevent fail and attempt to move spawns outside of things with collision (walls, rocks, etc.)
+				FActorSpawnParameters SpawnParams;
+				SpawnParams.bNoFail = true;
+				SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+                // spawn with default quantity
+				const int32 ItemQuantity = ItemClass->GetDefaultObject<UBaseItem>()->GetQuantity();
+
+                // spawn on the spawn loaction with offset taken into account
+				FTransform SpawnTransform = GetActorTransform();
+				SpawnTransform.AddToTranslation(LocationOffset);
+
+                // spawn the pickups and bind that item to OnItemTaken, which will trigger the timer to spawn a new item
+				APickupBase* Pickup = GetWorld()->SpawnActor<APickupBase>(PickupClass, SpawnTransform, SpawnParams);
+				Pickup->InitializePickup(ItemClass, ItemQuantity);
+				Pickup->OnDestroyed.AddUniqueDynamic(this, &AItemSpawn::OnItemTaken);
+
+                // add item to array of spawned pickups
+				SpawnedPickups.Add(Pickup);
+
+                // create the angle offset for the items based upon total number of spawns, to create circular spawn
+				Angle += (PI * 2.f) / LootRow->Items.Num();
+			}
+        }
+    }
+}
+
+// TODO:
+// May want a system that after a player interacts with any of the spawns it 
+// kicks off a timer and eventually deletes and respawns items in this location
+void AItemSpawn::OnItemTaken(AActor* DestroyedActor) 
+{
+    // check that server is taking this acction
+    if (HasAuthority())
+	{
+		// remove the pickup from the array
+        SpawnedPickups.Remove(DestroyedActor);
+
+		// if all pickups were taken from this pickup location, queue a respawn
+		if (SpawnedPickups.Num() <= 0)
+		{
+			GetWorldTimerManager().SetTimer(TimerHandle_RespawnItem, this, &AItemSpawn::SpawnItem, FMath::RandRange(RespawnRange.GetMin(), RespawnRange.GetMax()), false);
+		}
+	}
+}
--- World/ItemSpawn.h
+++ World/ItemSpawn.h
@@ -25,3 +25,57 @@ public:
 
 };
 
+// from video
+//UCLASS(ClassGroup = (Items), Blueprintable, Abstract)
+UCLASS()
+class TROLLED_API AItemSpawn : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	AItemSpawn();
+
+	// data table used to define what can items can be ground spawn
+	UPROPERTY(EditAnywhere, Category = "Loot")
+	class UDataTable* LootTable;
+
+	// Because pickups use a Blueprint base, we use a UPROPERTY to select it
+	UPROPERTY(EditDefaultsOnly, Category = "Loot")
+	TSubclassOf<class APickupBase> PickupClass;
+
+	// range of time used for randomly respawning a new pickup 
+	UPROPERTY(EditDefaultsOnly, Category = "Loot")
+	FIntPoint RespawnRange;
+
+	//The item(s) to spawn
+	UPROPERTY(EditDefaultsOnly, Category = "Loot")
+	TArray<TSubclassOf<class UBaseItem>> Items;
+
+	//The percentage chance of spawning this item if we hit it on the roll
+	UPROPERTY(EditDefaultsOnly, Category = "Loot", meta = (ClampMin = 0.001, ClampMax = 1.0))
+	float Probability = 1.f;
+
+protected:
+
+	// timer handle used for keeping track of time before a respawn
+	FTimerHandle TimerHandle_RespawnItem;
+
+	// array of current pickups that exist, used to prevent new items  
+	// being spawned in a location that isnt available
+	UPROPERTY()
+	TArray<AActor*> SpawnedPickups;
+
+	// on BeginPlay spawn the pickups in the world
+	virtual void BeginPlay() override;
+	
+	// spawns the item in the world
+	UFUNCTION()
+	void SpawnItem();
+
+	// This is bound to the item being destroyed, so we can queue up another item to be spawned in
+	UFUNCTION()
+	void OnItemTaken(AActor* DestroyedActor);
+
+};
+
