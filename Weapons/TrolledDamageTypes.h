fbd3b15964edacd84b9cf87ddbc76df42c435ad1
Eric Born <eric.born85@gmail.com>
2020-08-10 16:11:41 -0700

    Create damage types

---
+++ Weapons/TrolledDamageTypes.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "TrolledDamageTypes.generated.h"
+
+UCLASS()
+class TROLLED_API UTrolledDamageTypes : public UDamageType
+{
+	GENERATED_BODY()
+	
+};
+
+UCLASS()
+class UTrolledDamageBase : public UDamageType
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class UWeaponDamage : public UTrolledDamageBase
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class UMeleeDamage : public UTrolledDamageBase
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class UExplosiveDamage : public UTrolledDamageBase
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class UVehicleDamage : public UTrolledDamageBase
+{
+	GENERATED_BODY()
+};
